20cb58bf4942f7ea174a18d1bce2e0eb63b35c9b
Yevhen Sahidulin <sagiduline@mydigicode.com>
2018-12-24 11:07:58 +0200

    Initial Commit

---
+++ YESBluetoothDeviceManager/YESBluetoothDeviceManager.h
@@ -0,0 +1,19 @@
+//
+//  YESBluetoothDeviceManager.h
+//  YESBluetoothDeviceManager
+//
+//  Created by Yevhen Sahidulin on 12/24/18.
+//  Copyright © 2018 Yevhen Sahidulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YESBluetoothDeviceManager.
+FOUNDATION_EXPORT double YESBluetoothDeviceManagerVersionNumber;
+
+//! Project version string for YESBluetoothDeviceManager.
+FOUNDATION_EXPORT const unsigned char YESBluetoothDeviceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YESBluetoothDeviceManager/PublicHeader.h>
+
+
